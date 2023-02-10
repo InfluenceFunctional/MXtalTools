009e7b799dc52fbb7069cfe9016e67dd4d7357fe
InfluenceFunctional <mjakilgour@gmail.com>
2023-02-10 11:56:45 -0500

    > test

--- crystal_modeller.py
+++ crystal_modeller.py
@@ -542,7 +542,7 @@ class Modeller():
             g_loss_record.extend(regression_losses_list.cpu().detach().numpy())  # loss distribution
 
             if update_gradients:
-                g_optimizer.zero_grad(set_to_none=True)  # reset gradients from previous passes
+                g_optimizer.zero_grad(set_to_none=False)  # reset gradients from previous passes
                 g_loss.backward()  # back-propagation
                 g_optimizer.step()  # update parameters
 
@@ -611,8 +611,8 @@ class Modeller():
         self.n_samples_in_grad_buffer = 0
 
         if update_gradients:
-            g_optimizer.zero_grad(set_to_none=True)
-            d_optimizer.zero_grad(set_to_none=True)
+            g_optimizer.zero_grad(set_to_none=False)
+            d_optimizer.zero_grad(set_to_none=False)
 
         for i, data in enumerate(tqdm.tqdm(dataLoader, miniters=int(len(dataLoader) / 10))):
 
@@ -850,7 +850,7 @@ class Modeller():
                 d_loss_record.extend(d_losses.cpu().detach().numpy())  # overall loss distribution
 
                 # if update_gradients:
-                #     d_optimizer.zero_grad(set_to_none=True)  # reset gradients from previous passes
+                #     d_optimizer.zero_grad(set_to_none=False)  # reset gradients from previous passes
                 #     d_loss = d_loss / data.num_nodes  # normalize the loss by mean graph size
                 #     d_loss.backward()  # back-propagation
                 #     torch.nn.utils.clip_grad_norm_(discriminator.parameters(), self.config.gradient_norm_clip)  # gradient clipping
@@ -865,11 +865,11 @@ class Modeller():
                         self.n_samples_in_grad_buffer += data.num_graphs
                         if (self.n_samples_in_grad_buffer > self.config.accumulate_batch_size) or last_batch:
                             d_optimizer.step()
-                            d_optimizer.zero_grad(set_to_none=True)  # reset gradients from previous passes
+                            d_optimizer.zero_grad(set_to_none=False)  # reset gradients from previous passes
                             self.n_samples_in_grad_buffer = 0
                     else:
                         d_optimizer.step()  # update parameters
-                        d_optimizer.zero_grad(set_to_none=True)  # reset gradients from previous passes
+                        d_optimizer.zero_grad(set_to_none=False)  # reset gradients from previous passes
 
                 epoch_stats_dict['generated cell parameters'].extend(generated_samples_i.cpu().detach().numpy())
                 epoch_stats_dict['final generated cell parameters'].extend(generated_samples)
@@ -907,11 +907,11 @@ class Modeller():
                     self.n_samples_in_grad_buffer += data.num_graphs
                     if (self.n_samples_in_grad_buffer > self.config.accumulate_batch_size) or last_batch:
                         g_optimizer.step()
-                        g_optimizer.zero_grad(set_to_none=True)  # reset gradients from previous passes
+                        g_optimizer.zero_grad(set_to_none=False)  # reset gradients from previous passes
                         self.n_samples_in_grad_buffer = 0
                 else:
                     g_optimizer.step()  # update parameters
-                    g_optimizer.zero_grad(set_to_none=True)  # reset gradients from previous passes
+                    g_optimizer.zero_grad(set_to_none=False)  # reset gradients from previous passes
 
         else:
             g_err.append(np.zeros(1))
