2e3d9163d1091f761686914c0ba8398de28e3eb5
InfluenceFunctional <mjakilgour@gmail.com>
2023-11-20 13:59:43 -0500

    :> functional 3D autoencoder for big tests

--- autoencoder/configs.py
+++ autoencoder/configs.py
@@ -71,7 +71,7 @@ dev = {'run_name': 'dev',
 configs = []
 
 base_config = {'run_name': 'base',
-               'experiment_tag': ['battery_1'],
+               'experiment_tag': ['battery_2'],
                'device': 'cuda',
                'seed': 1234,
                'training_iterations': int(1e8),
@@ -83,7 +83,7 @@ base_config = {'run_name': 'base',
                'sigma': 0.1,  # larger increases overlaps
                'sigma_lambda': 0.95,
                'sigma_threshold': 0.1,
-               'type_distance_scaling': 0.1,  # larger decreases overlaps
+               'type_distance_scaling': 0.25,  # larger decreases overlaps
                'overlap_type': 'gaussian',
                'log_reconstruction': False,
                'do_training': True,
@@ -409,3 +409,110 @@ aa = 1
 #     'sigma': [0.01],
 #     'overlap_type': ['gaussian'],
 # }
+
+
+#
+#
+# base_config = {'run_name': 'base',
+#                'experiment_tag': ['battery_1'],
+#                'device': 'cuda',
+#                'seed': 1234,
+#                'training_iterations': int(1e8),
+#                'min_num_training_steps': 10000,
+#                'convergence_history': 50,
+#                'convergence_eps': 1e-3,
+#
+#                # loss settings
+#                'sigma': 0.1,  # larger increases overlaps
+#                'sigma_lambda': 0.95,
+#                'sigma_threshold': 0.1,
+#                'type_distance_scaling': 0.1,  # larger decreases overlaps
+#                'overlap_type': 'gaussian',
+#                'log_reconstruction': False,
+#                'do_training': True,
+#                'cart_dimension': 3,
+#                'train_nodewise_type_loss': True,
+#                'train_reconstruction_loss': True,
+#                'train_centroids_loss': True,
+#                'train_type_confidence_loss': False,
+#                'train_num_points_loss': False,
+#                'train_encoding_type_loss': False,
+#
+#                # dataset & dataloading
+#                'batch_size_min': 2,
+#                'batch_size_max': 400,
+#                'num_fc_nodes': 100,
+#                'batch_size_increment': 1,
+#                'mean_num_points': 5,
+#                'num_points_spread': 1,  # more like a sigma
+#                'max_num_points': 10,
+#                'min_num_points': 2,
+#                'points_spread': 1,  # max value
+#                'max_point_types': 5,
+#
+#                # lrs
+#                'encoder_lr': 1e-4,
+#                'decoder_lr': 1e-4,
+#                'lr_lambda': 0.975,
+#                'lr_timescale': 500,
+#
+#                # encoder
+#                'encoder_aggregator': 'combo',
+#                'encoder_num_layers': 1,
+#                'encoder_num_fc_layers': 4,
+#                'encoder_embedding_depth': 128,
+#                'encoder_num_nodewise_fcs': 4,
+#                'encoder_fc_norm': None,
+#                'encoder_graph_norm': None,
+#                'encoder_dropout': 0,
+#
+#                # decoder
+#                'decoder_num_layers': 1,
+#                'decoder_embedding_depth': 32,
+#                'decoder_fc_norm': None,
+#                'decoder_dropout': 0,
+#
+#                # paths
+#                #'run_directory': r'C:\Users\mikem\crystals\CSP_runs',
+#                'run_directory': '/scratch/mk8347/csd_runs/',
+#                #'save_directory': 'D:/crystals_extra/',
+#                'save_directory': '/scratch/mk8347/csd_runs/',
+#                'checkpoint_path': None,  # r'C:\Users\mikem\crystals\CSP_runs\models\cluster/test1_autoencoder_ckpt_11_12',
+#                }
+#
+# search_space = {
+#     'encoder_embedding_depth': [256, 512],
+#     'decoder_embedding_depth': [16, 32, 64],
+#     'num_fc_nodes': [50, 100, 200],
+#     'encoder_dropout': [0, 0.1],
+#     'decoder_dropout': [0, 0.1],
+#     'encoder_graph_norm': [None, 'graph layer'],
+#     'decoder_graph_norm': [None, 'layer'],
+#     'encoder_fc_norm': [None, 'layer'],
+#     'encoder_num_layers': [1, 2],
+#     'decoder_num_layers': [1, 2],
+#     'encoder_num_fc_layers': [1, 2, 4],
+#     'encoder_num_nodewise_fcs': [1, 2, 4],
+#     'encoder_lr': [1e-4, 1e-5],
+#     'decoder_lr': [1e-4, 1e-5],
+#     'encoder_aggregator': ['max', 'combo'],
+#     'overlap_type': ['gaussian'],
+#     'train_typewise_node_loss': [True, False],
+# }
+#
+# np.random.seed(0)
+# num_tests = 1000
+# randints = np.stack(
+#     [np.concatenate(
+#         [np.random.randint(0, len(values), size=1) for values in search_space.values()]
+#     ) for _ in range(num_tests)]
+# )
+#
+# for i in range(num_tests):
+#     new_config = copy(base_config)
+#     new_config['run_name'] = f"{new_config['experiment_tag'][0]}_{i}"
+#
+#     for ind, (key, values) in enumerate(search_space.items()):
+#         new_config[key] = values[randints[i, ind]]
+#
+#     configs.append(new_config)
