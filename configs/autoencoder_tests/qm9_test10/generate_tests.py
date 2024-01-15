1ab7c4895291ea35bbe9b171db2a818eca6f7f77
InfluenceFunctional <mjakilgour@gmail.com>
2024-01-15 09:12:35 -0500

    :> clustering / reporting :> more run configs

--- configs/autoencoder_tests/qm9_test10/generate_tests.py
+++ configs/autoencoder_tests/qm9_test10/generate_tests.py
@@ -13,14 +13,14 @@ base_config = load_yaml('base.yaml')
 """
 
 configs = [
-    [1, 171, 171, 1, 1, 1],  # 0
-    [0, 513, 171, 1, 1, 1],  # 1
-    [0, 513, 171, 4, 4, 1],  # 2
-    [2, 513, 171, 1, 4, 9],  # 3
-    [4, 513, 171, 1, 4, 9],  # 4
-    [4, 513, 171, 1, 8, 9],  # 5
-    [4, 513, 513, 1, 8, 27],  # 6
-    [8, 513, 513, 1, 8, 27],  # 7
+    [1, 171, 171, 1, 1, 1, 1e-4],  # 0
+    [0, 513, 171, 1, 1, 1, 1e-4],  # 1
+    [0, 513, 171, 4, 4, 1, 1e-4],  # 2
+    [2, 513, 171, 1, 4, 9, 1e-5],  # 3  wacky explosion
+    [4, 513, 171, 1, 4, 9, 1e-5],  # 4 CUDA error
+    [4, 513, 171, 1, 8, 9, 1e-5],  # 5 NaN out
+    [4, 513, 513, 1, 8, 27, 1e-5],  # 6 NaN out
+    [8, 513, 513, 1, 8, 27, 1e-5],  # 7 NaN out
 ]
 
 ind = 0
@@ -33,6 +33,8 @@ for ii in range(len(configs)):
     config['autoencoder']['model']['nodewise_fc_layers'] = configs[ii][3]
     config['autoencoder']['model']['num_decoder_layers'] = configs[ii][4]
     config['autoencoder']['model']['num_attention_heads'] = configs[ii][5]
+    config['autoencoder']['optimizer']['encoder_init_lr'] = configs[ii][6]
+
 
     with open(str(ind) + '.yaml', 'w') as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
