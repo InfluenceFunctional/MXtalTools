08e8cddb3745fd8b71451d2cc889ab64334c9eb3
InfluenceFunctional <mjakilgour@gmail.com>
2024-01-28 14:40:56 -0500

    :> new traj analysis

--- bulk_molecule_classification/multi_run_analysis.py
+++ bulk_molecule_classification/multi_run_analysis.py
@@ -2,13 +2,14 @@ import os
 import pandas as pd
 import numpy as np
 import warnings
+from tqdm import tqdm
 
 # from reporting.cluster_figs import cluster_property_heatmap, collate_property_over_multiple_runs, plot_classifier_pies
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 from bulk_molecule_classification.classifier_constants import nic_ordered_class_names, identifier2form
-from bulk_molecule_classification.traj_analysis_figs import plot_classifier_pies, cluster_property_heatmap
+# from bulk_molecule_classification.traj_analysis_figs import plot_classifier_pies, cluster_property_heatmap
 from common.utils import delete_from_dataframe
 
 warnings.filterwarnings("ignore", category=FutureWarning)  # ignore numpy error
@@ -19,14 +20,21 @@ files = os.listdir()
 results_dicts = [file for file in files if 'analysis' in file and 'crystal_in_melt_test8' in file]
 
 dfs = []
-for ind, dict_path in enumerate(results_dicts):
+useful_keys = ['Molecule_Temperature', 'Molecule_Type_Prediction', 'Centroid Radii']
+for ind, dict_path in tqdm(enumerate(results_dicts)):
+    dumps_dir = f'D:/crystals_extra/classifier_training/crystal_in_melt_test8/{int(dict_path.split("_")[-3])}/'
+    if os.path.exists(dumps_dir + 'run_config.npy'):
+        run_config = np.load(dumps_dir + 'run_config.npy', allow_pickle=True).item()
+
     results_dict = np.load(dict_path, allow_pickle=True).item()
-    ldict = {key: [value] for key, value in results_dict.items()}
-    for key, value in results_dict['run_config'].items():
+    ldict = {key: [results_dict[key]] for key in useful_keys}
+    for key, value in run_config.items():
         ldict[key] = [value]
 
-    for key, value in results_dict['eval_config'].items():
-        ldict[key] = [value]
+    ldict['time_steps'] = [np.asarray([time[0] for time in results_dict['Molecule_Time_Step']])]
+
+    # for key, value in results_dict['eval_config'].items():
+    #     ldict[key] = [value]
 
     dfs.append(pd.DataFrame().from_dict(ldict))
 
@@ -35,12 +43,6 @@ results_df.reset_index(drop=True, inplace=True)
 del (dfs)
 
 """crystal melts analysis"""
-# we want to know
-# for runs that completed
-# initial equil form fraction
-# post liquification form fraction
-# final form fraction
-
 # filter unfinished runs
 run_lengths = np.asarray([ts[-1] for ts in results_df['time_steps']])
 bad_inds = np.argwhere(run_lengths < run_lengths.max())[:, 0]
@@ -66,33 +68,33 @@ melt_finished_time = equil_time * 3  # finish melt
 melt_finished_time_ind = get_closest_prior_time(melt_finished_time, timesteps)
 final_time = timesteps[-1]
 final_time_ind = len(timesteps) - 1
-
-init_stability = np.zeros(len(results_df))
-melt_stability = np.zeros_like(init_stability)
-final_stability = np.zeros_like(melt_stability)
-stability_change = np.zeros_like(final_stability)
-
-for ind in range(len(results_df)):
-    identifier = results_df['structure_identifier'][ind]
-    form = identifier2form[identifier]
-    form_ind = form - 1
-    init_stability[ind] = results_df['inside_fraction'].iloc[ind][init_equil_time_ind, form_ind]
-    melt_stability[ind] = results_df['inside_fraction'].iloc[ind][melt_finished_time_ind, form_ind]
-    final_stability[ind] = results_df['inside_fraction'].iloc[ind][final_time_ind, form_ind]
-    stability_change[ind] = melt_stability[ind] - final_stability[ind]
-
-results_df['init_stability'] = init_stability
-results_df['melt_stability'] = melt_stability
-results_df['final_stability'] = final_stability
-results_df['stability_change'] = stability_change
-
-figs = []
-for property in ['init_stability', 'melt_stability', 'final_stability', 'stability_change']:
-    figs.append(cluster_property_heatmap(results_df,
-                                         property=property,
-                                         xaxis_title='max_sphere_radius',
-                                         yaxis_title='temperature',
-                                         ))
+#
+# init_stability = np.zeros(len(results_df))
+# melt_stability = np.zeros_like(init_stability)
+# final_stability = np.zeros_like(melt_stability)
+# stability_change = np.zeros_like(final_stability)
+#
+# for ind in range(len(results_df)):
+#     identifier = results_df['structure_identifier'][ind]
+#     form = identifier2form[identifier]
+#     form_ind = form - 1
+#     init_stability[ind] = results_df['inside_fraction'].iloc[ind][init_equil_time_ind, form_ind]
+#     melt_stability[ind] = results_df['inside_fraction'].iloc[ind][melt_finished_time_ind, form_ind]
+#     final_stability[ind] = results_df['inside_fraction'].iloc[ind][final_time_ind, form_ind]
+#     stability_change[ind] = melt_stability[ind] - final_stability[ind]
+#
+# results_df['init_stability'] = init_stability
+# results_df['melt_stability'] = melt_stability
+# results_df['final_stability'] = final_stability
+# results_df['stability_change'] = stability_change
+#
+# figs = []
+# for property in ['init_stability', 'melt_stability', 'final_stability', 'stability_change']:
+#     figs.append(cluster_property_heatmap(results_df,
+#                                          property=property,
+#                                          xaxis_title='max_sphere_radius',
+#                                          yaxis_title='temperature',
+#                                          ))
 
 aa = 0
 
@@ -103,51 +105,56 @@ colors = n_colors('rgb(5,120,200)', 'rgb(250,50,5)', len(np.unique(results_df['t
 temps = np.sort(np.unique(results_df['temperature']))
 temp_dict = {temp: ind for ind, temp in enumerate(temps)}
 
-tstart = get_closest_prior_time(equil_time * 3, timesteps) + 1
-sigma=5
+sigma = 5
 
 fig = make_subplots(cols=2, rows=2, subplot_titles=['Raw Values', 'Normed against t=0'], horizontal_spacing=0.05, vertical_spacing=0.075)
 for temp in temps:
     for ind in range(len(results_df)):
+        identifier = results_df['structure_identifier'][ind]
+        form = identifier2form[identifier]
+        form_ind = form - 1
+        inside_radius = results_df['max_sphere_radius'][ind]
+
         if results_df['temperature'].iloc[ind] == temp:
+            tstart = get_closest_prior_time(equil_time * 2.5, timesteps) + 1
+
+            inside_inds = [np.argwhere(results_df['Centroid Radii'].iloc[ind][t] < inside_radius)[:, 0] for t in range(tstart, len(timesteps))]
+            inside_count = np.asarray([
+                np.sum(results_df['Molecule_Type_Prediction'].iloc[ind][t][inside_inds[t - tstart]].argmax(1) == 3)
+                for t in range(tstart, len(timesteps))
+            ])
             x = results_df['time_steps'].iloc[ind][tstart:]
-            y = results_df['inside_number'].iloc[ind][tstart:, form_ind]
-            fig.add_scatter(x=x, y=gaussian_filter1d(y, sigma=sigma),
+            y = inside_count
+            fig.add_scatter(x=x / 1e6, y=gaussian_filter1d(y, sigma=sigma, mode='nearest'),
                             name=str(results_df['temperature'].iloc[ind]),
                             legendgroup=str(results_df['temperature'].iloc[ind]),
                             line_color=colors[temp_dict[results_df['temperature'].iloc[ind]]],
                             showlegend=ind % 2 == 0, row=1, col=1)
-for temp in temps:
-    for ind in range(len(results_df)):
-        if results_df['temperature'].iloc[ind] == temp:
-            x = results_df['time_steps'].iloc[ind][tstart:]
-            y = results_df['inside_number'].iloc[ind][tstart:, form_ind] / results_df['inside_number'].iloc[ind][tstart, form_ind]
-            fig.add_scatter(x=x, y=gaussian_filter1d(y, sigma=sigma),
+
+            y = inside_count / inside_count[0]
+            fig.add_scatter(x=x / 1e6, y=gaussian_filter1d(y, sigma=sigma, mode='nearest'),
                             name=str(results_df['temperature'].iloc[ind]),
                             legendgroup=str(results_df['temperature'].iloc[ind]),
                             line_color=colors[temp_dict[results_df['temperature'].iloc[ind]]],
                             showlegend=False, row=1, col=2)
 
-
-tstart = 0
-sigma=5
-
-for temp in temps:
-    for ind in range(len(results_df)):
-        if results_df['temperature'].iloc[ind] == temp:
+            '''repeat for full trajectory'''
+            tstart = 0
+            inside_inds = [np.argwhere(results_df['Centroid Radii'].iloc[ind][t] < inside_radius)[:, 0] for t in range(tstart, len(timesteps))]
+            inside_count = np.asarray([
+                np.sum(results_df['Molecule_Type_Prediction'].iloc[ind][t][inside_inds[t - tstart]].argmax(1) == 3)
+                for t in range(tstart, len(timesteps))
+            ])
             x = results_df['time_steps'].iloc[ind][tstart:]
-            y = results_df['inside_number'].iloc[ind][tstart:, form_ind]
-            fig.add_scatter(x=x, y=gaussian_filter1d(y, sigma=sigma),
+            y = inside_count
+            fig.add_scatter(x=x / 1e6, y=gaussian_filter1d(y, sigma=sigma, mode='nearest'),
                             name=str(results_df['temperature'].iloc[ind]),
                             legendgroup=str(results_df['temperature'].iloc[ind]),
                             line_color=colors[temp_dict[results_df['temperature'].iloc[ind]]],
-                            showlegend=False, row=2, col=1)
-for temp in temps:
-    for ind in range(len(results_df)):
-        if results_df['temperature'].iloc[ind] == temp:
-            x = results_df['time_steps'].iloc[ind][tstart:]
-            y = results_df['inside_number'].iloc[ind][tstart:, form_ind] / results_df['inside_number'].iloc[ind][tstart, form_ind]
-            fig.add_scatter(x=x, y=gaussian_filter1d(y, sigma=sigma),
+                            showlegend=ind % 2 == 0, row=2, col=1)
+
+            y = inside_count / inside_count[0]
+            fig.add_scatter(x=x / 1e6, y=gaussian_filter1d(y, sigma=sigma, mode='nearest'),
                             name=str(results_df['temperature'].iloc[ind]),
                             legendgroup=str(results_df['temperature'].iloc[ind]),
                             line_color=colors[temp_dict[results_df['temperature'].iloc[ind]]],
@@ -156,7 +163,12 @@ for temp in temps:
 fig.update_xaxes(title='Time (ns)')
 fig.update_yaxes(title='Num Molecules')
 
+for times in [equil_time * (i + 1) for i in range(3)]:
+    fig.add_vline(x=times/1e6, line_color='black', row=2, col=1)
+    fig.add_vline(x=times/1e6, line_color='black', row=2, col=2)
+
 fig.show()
+aa = 1
 
 #
 #
--- bulk_molecule_classification/workflows.py
+++ bulk_molecule_classification/workflows.py
@@ -148,149 +148,174 @@ def classifier_evaluation(config, classifier, train_loader, test_loader,
 
 def trajectory_analysis(config, classifier, wandb, device, dumps_dir):
 
-    from bulk_molecule_classification.ovito_utils import write_ovito_xyz
     dataset_name = '_'.join(dumps_dir.split('/')[-3:])
     datasets_path = config['datasets_path']
     dataset_path = f'{datasets_path}{dataset_name}.pkl'
     output_dict_path = config['results_path'] + dataset_name + '_analysis'
 
-    if True:  # not os.path.exists(output_dict_path + '.npy'):
-        if not os.path.exists(dataset_path):
-            made_dataset = generate_dataset_from_dumps([dumps_dir], dataset_path)
-
-            if not made_dataset:
-                print(f'{dumps_dir} does not contain valid dump to analyze')
-                return False
-
-        os.chdir(config['runs_path'])  # todo move this
-
+    if not os.path.exists(output_dict_path + '.npy'):
+        loader, run_config = collect_trajectory_dataloader(config, dataset_path, dumps_dir)
+        results_dict = classify_trajectory(classifier, config, device, loader)
+        os.chdir(config['results_path'])
+        sorted_molwise_results_dict, time_steps = process_classification_results(config, loader, output_dict_path, results_dict)
+    else:
+        os.chdir(config['results_path'])
         if os.path.exists(dumps_dir + 'run_config.npy'):
             run_config = np.load(dumps_dir + 'run_config.npy', allow_pickle=True).item()
         else:
             run_config = None
+        sorted_molwise_results_dict = np.load(output_dict_path + '.npy', allow_pickle=True).item()
+        time_steps = np.asarray([time[0] for time in sorted_molwise_results_dict['Molecule_Time_Step']])
 
-        _, loader = collect_to_traj_dataloaders(config['mol_num_atoms'],
-                                                dataset_path, config['dataset_size'],
-                                                batch_size=1, temperatures=None,
-                                                conv_cutoff=config['conv_cutoff'],
-                                                test_fraction=1, shuffle=False, filter_early=False,
-                                                early_only=False,
-                                                run_config=run_config,
-                                                pare_to_cluster=False if 'interface' in dataset_path else True,
-                                                single_trajectory=True)
-        results_dict = None
-        classifier.train(False)
-        with torch.no_grad():
-            classifier.eval()
-            for step, data in enumerate(tqdm(loader)):
-                sample = data.to(device)
-                (output, latents), embeddings = classifier(sample, return_latent=True, return_embedding=True)
-                results_dict = record_step_results(results_dict, output, sample, data, latents, embeddings, step, config)
+    interface_mode = check_and_process_interface(dumps_dir, sorted_molwise_results_dict)
 
-        for key in results_dict.keys():
-            try:
-                results_dict[key] = np.concatenate(results_dict[key], axis=0)
-            except:
-                results_dict[key] = np.stack(results_dict[key])
-
-        os.chdir(config['results_path'])
-
-        results_dict['Atomwise_Sample_Index'] = results_dict['Sample_Index'].repeat(config['mol_num_atoms'])
-        results_dict['Type_Prediction_Choice'] = np.argmax(results_dict['Type_Prediction'], axis=-1)  # argmax sample
-        results_dict['Type_Prediction_Choice'] = np.asarray([form2index[tgt] for tgt in results_dict['Type_Prediction_Choice']])
-        results_dict['Type_Prediction_Confidence'] = np.amax(results_dict['Type_Prediction'], axis=-1)  # argmax sample
-
-        sorted_molwise_results_dict, time_steps = process_trajectory_results_dict(results_dict, loader, config['mol_num_atoms'])
-        del results_dict
-
-        if dumps_dir == r'D:/crystals_extra/classifier_training/urea_melt_interface_T200':  # collect predictions to form I and IV or 'other'
-            interface_mode = True
-            for step in tqdm(range(len(sorted_molwise_results_dict['Molecule_Type_Prediction']))):
-                orig_pred = sorted_molwise_results_dict['Molecule_Type_Prediction'][step]
-                unknowns = orig_pred[:, [0, 1, 2, 4, 6]].sum(1)[:, None]
-                new_pred = np.concatenate([orig_pred[:, [3, 5]], unknowns], axis=1)
-                sorted_molwise_results_dict['Molecule_Type_Prediction'][step] = new_pred
-
-                orig_choice = sorted_molwise_results_dict['Molecule_Type_Prediction_Choice'][step]
-                orig_choice[orig_choice == 2] = 1  # form IV to index 1
-                for ind in range(8):
-                    if ind != 0 and ind != 1:
-                        orig_choice[orig_choice == ind] = 2
-
-                sorted_molwise_results_dict['Molecule_Type_Prediction_Choice'][step] = orig_choice
-        else:
-            interface_mode = False
-
-        np.save(output_dict_path, sorted_molwise_results_dict)
+    try:
+        from bulk_molecule_classification.ovito_utils import write_ovito_xyz
+        dataset_name = '_'.join(dumps_dir.split('/')[-3:])
 
         write_ovito_xyz(sorted_molwise_results_dict['Coordinates'],
                         sorted_molwise_results_dict['Atom_Types'],
                         sorted_molwise_results_dict['Molecule_Type_Prediction_Choice'], filename=dataset_name + '_prediction')  # write a trajectory
+        #
+        # write_ovito_xyz(sorted_molwise_results_dict['Coordinates'],
+        #                 sorted_molwise_results_dict['Atom_Types'],
+        #                 sorted_molwise_results_dict['Molecule_Type_Prediction'], filename=dataset_name + '_all_probs')  # write a trajectory
+    except:
+        print("Ovito write error")
 
-        write_ovito_xyz(sorted_molwise_results_dict['Coordinates'],
-                        sorted_molwise_results_dict['Atom_Types'],
-                        sorted_molwise_results_dict['Molecule_Type_Prediction'], filename=dataset_name + '_all_probs')  # write a trajectory
+    inside_radius, run_config = get_inside_radius(dumps_dir, run_config)
 
-        if os.path.exists(dumps_dir + 'run_config.npy'):
-            run_config = np.load(dumps_dir + 'run_config.npy', allow_pickle=True).item()
-            if 'crystal' in dumps_dir and 'melt' in dumps_dir:
-                inside_radius = run_config['max_sphere_radius']
-            else:
-                inside_radius = None
+    fig, fig2, traj_dict = classifier_trajectory_analysis_fig(
+        sorted_molwise_results_dict, time_steps,
+        'urea' if config['mol_num_atoms'] == 8 else 'nicotinamide',
+        inside_radius=inside_radius, interface_mode=interface_mode)
+
+    if os.path.exists(dumps_dir + 'run_config.npy'):
+        fig.update_layout(
+            title=f"Form {identifier2form[run_config['structure_identifier']]}, "
+                  f"Cluster Radius {run_config['max_sphere_radius']}A, "
+                  f"Temperature {run_config['temperature']}K")
+        fig2.update_layout(
+            title=f"Form {identifier2form[run_config['structure_identifier']]}, "
+                  f"Cluster Radius {run_config['max_sphere_radius']}A, "
+                  f"Temperature {run_config['temperature']}K")
+
+    elif 'urea_interface' in dumps_dir:
+        fig.update_layout(
+            title="Urea Interface")
+    else:
+        print("Missing trajectory config")
+
+    fig2 = radial_form_timeseries(fig, inside_radius, sorted_molwise_results_dict, time_steps)
+
+    fig.write_image(f"{dataset_name}_Trajectory_Analysis.png", scale=4)
+    fig2.write_image(f"{dataset_name}_Radial_Stability.png", width=1920, height=1080)
+    wandb.log({f"{dataset_name} Trajectory Analysis": fig})
+
+
+def radial_form_timeseries(fig, inside_radius, sorted_molwise_results_dict, time_steps):
+    # type density vs radius over time
+    from plotly.subplots import make_subplots
+    import plotly.graph_objects as go
+    from scipy.ndimage import gaussian_filter1d
+    timepoints = np.linspace(0, len(time_steps) - 1, 36).astype(int)
+    fig = make_subplots(rows=6, cols=6, subplot_titles=[f't={timepoint}' for timepoint in timepoints])
+    for ind in range(36):
+        row = ind // 6 + 1
+        col = ind % 6 + 1
+        ind = timepoints[ind]
+        radii = sorted_molwise_results_dict['Centroid Radii'][ind]
+        f4_frac = sorted_molwise_results_dict['Molecule_Type_Prediction'][ind][:, 3]
+        sort_inds = np.argsort(radii)[::-1]
+        fig.add_trace(go.Scatter(x=radii[sort_inds], y=gaussian_filter1d(f4_frac[sort_inds], sigma=5), showlegend=False), row=row, col=col)
+        fig.add_vline(x=inside_radius, line_color='black', row=row, col=col)
+    fig.update_xaxes(title='Radius')
+    fig.update_yaxes(title='Form 4 Fraction')
+    fig.update_yaxes(range=[-0.05, 1.05])
+    return fig
+
+
+def get_inside_radius(dumps_dir, run_config):
+    if os.path.exists(dumps_dir + 'run_config.npy'):
+        run_config = np.load(dumps_dir + 'run_config.npy', allow_pickle=True).item()
+        if 'crystal' in dumps_dir and 'melt' in dumps_dir:
+            inside_radius = run_config['max_sphere_radius']
         else:
             inside_radius = None
+    else:
+        inside_radius = None
+    return inside_radius, run_config
 
-        fig, fig2, traj_dict = classifier_trajectory_analysis_fig(
-            sorted_molwise_results_dict, time_steps,
-            'urea' if config['mol_num_atoms'] == 8 else 'nicotinamide',
-            inside_radius=inside_radius, interface_mode=interface_mode)
 
-        """ type density vs radius over time
-        from plotly.subplots import make_subplots
-        import plotly.graph_objects as go
-        from scipy.ndimage import gaussian_filter1d
+def check_and_process_interface(dumps_dir, sorted_molwise_results_dict):
+    if dumps_dir == r'D:/crystals_extra/classifier_training/urea_melt_interface_T200':  # collect predictions to form I and IV or 'other'
+        interface_mode = True
+        for step in tqdm(range(len(sorted_molwise_results_dict['Molecule_Type_Prediction']))):
+            orig_pred = sorted_molwise_results_dict['Molecule_Type_Prediction'][step]
+            unknowns = orig_pred[:, [0, 1, 2, 4, 6]].sum(1)[:, None]
+            new_pred = np.concatenate([orig_pred[:, [3, 5]], unknowns], axis=1)
+            sorted_molwise_results_dict['Molecule_Type_Prediction'][step] = new_pred
 
-        timepoints = np.linspace(0, 200, 24).astype(int)
-        
-        fig = make_subplots(rows=4, cols=6, subplot_titles=[f't={timepoint}' for timepoint in timepoints])
-        for ind in range(24):
-            row = ind // 6 + 1
-            col = ind % 6 + 1
-            ind = timepoints[ind]
-            print(row)
-            print(col)
-            radii = sorted_molwise_results_dict['Centroid Radii'][ind]
-            f4_frac = sorted_molwise_results_dict['Molecule_Type_Prediction'][ind][:, 3]
-            sort_inds = np.argsort(radii)[::-1]
-            fig.add_trace(go.Scatter(x=radii[sort_inds], y=gaussian_filter1d(f4_frac[sort_inds],sigma=5)), row=row, col=col)
-        fig.update_xaxes(title='Radius')
-        fig.update_yaxes(title='Form 4 Fraction')
-        fig.update_yaxes(range=[-0.05, 1.05])
-        fig.show()
-        """
+            orig_choice = sorted_molwise_results_dict['Molecule_Type_Prediction_Choice'][step]
+            orig_choice[orig_choice == 2] = 1  # form IV to index 1
+            for ind in range(8):
+                if ind != 0 and ind != 1:
+                    orig_choice[orig_choice == ind] = 2
 
-        if os.path.exists(dumps_dir + 'run_config.npy'):
-            fig.update_layout(
-                title=f"Form {identifier2form[run_config['structure_identifier']]}, "
-                      f"Cluster Radius {run_config['max_sphere_radius']}A, "
-                      f"Temperature {run_config['temperature']}K")
-            fig2.update_layout(
-                title=f"Form {identifier2form[run_config['structure_identifier']]}, "
-                      f"Cluster Radius {run_config['max_sphere_radius']}A, "
-                      f"Temperature {run_config['temperature']}K")
+            sorted_molwise_results_dict['Molecule_Type_Prediction_Choice'][step] = orig_choice
+    else:
+        interface_mode = False
+    return interface_mode
 
-        elif 'urea_interface' in dumps_dir:
-            fig.update_layout(
-                title="Urea Interface")
-            run_config = None
-        else:
-            run_config = None
-            print("Missing trajectory config")
 
-        traj_analysis = traj_dict
-        traj_analysis['run_config'] = run_config
-        traj_analysis['eval_config'] = config
+def collect_trajectory_dataloader(config, dataset_path, dumps_dir):
+    if not os.path.exists(dataset_path):
+        made_dataset = generate_dataset_from_dumps([dumps_dir], dataset_path)
 
-        fig.write_image(f"{dataset_name}_Trajectory_Analysis.png", scale=4)
-        fig2.write_image(f"{dataset_name}_Combined_Trajectory_Analysis.png", scale=4)
-        wandb.log({f"{dataset_name} Trajectory Analysis": fig})
+        if not made_dataset:
+            print(f'{dumps_dir} does not contain valid dump to analyze')
+            assert False
+    os.chdir(config['runs_path'])
+    if os.path.exists(dumps_dir + 'run_config.npy'):
+        run_config = np.load(dumps_dir + 'run_config.npy', allow_pickle=True).item()
+    else:
+        run_config = None
+    _, loader = collect_to_traj_dataloaders(config['mol_num_atoms'],
+                                            dataset_path, config['dataset_size'],
+                                            batch_size=1, temperatures=None,
+                                            conv_cutoff=config['conv_cutoff'],
+                                            test_fraction=1, shuffle=False, filter_early=False,
+                                            early_only=False,
+                                            run_config=run_config,
+                                            pare_to_cluster=False if 'interface' in dataset_path else True,
+                                            single_trajectory=True)
+    return loader, run_config
+
+
+def process_classification_results(config, loader, output_dict_path, results_dict):
+    results_dict['Atomwise_Sample_Index'] = results_dict['Sample_Index'].repeat(config['mol_num_atoms'])
+    results_dict['Type_Prediction_Choice'] = np.argmax(results_dict['Type_Prediction'], axis=-1)  # argmax sample
+    results_dict['Type_Prediction_Choice'] = np.asarray([form2index[tgt] for tgt in results_dict['Type_Prediction_Choice']])
+    results_dict['Type_Prediction_Confidence'] = np.amax(results_dict['Type_Prediction'], axis=-1)  # argmax sample
+    sorted_molwise_results_dict, time_steps = process_trajectory_results_dict(results_dict, loader, config['mol_num_atoms'])
+    del results_dict
+
+    np.save(output_dict_path, sorted_molwise_results_dict)  # todo reorganize this function
+    return sorted_molwise_results_dict, time_steps
+
+
+def classify_trajectory(classifier, config, device, loader):
+    results_dict = None
+    classifier.train(False)
+    with torch.no_grad():
+        classifier.eval()
+        for step, data in enumerate(tqdm(loader)):
+            sample = data.to(device)
+            (output, latents), embeddings = classifier(sample, return_latent=True, return_embedding=True)
+            results_dict = record_step_results(results_dict, output, sample, data, latents, embeddings, step, config)
+    for key in results_dict.keys():
+        try:
+            results_dict[key] = np.concatenate(results_dict[key], axis=0)
+        except:
+            results_dict[key] = np.stack(results_dict[key])
+    return results_dict
