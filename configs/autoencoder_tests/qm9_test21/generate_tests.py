6a02e04fbc6e5f15b11fddd091b44922b6930de0
InfluenceFunctional <mjakilgour@gmail.com>
2024-02-11 09:31:48 -0500

    :> architecture update :> switched to norm after linear as default :> more configs

--- configs/autoencoder_tests/qm9_test21/generate_tests.py
+++ configs/autoencoder_tests/qm9_test21/generate_tests.py
@@ -5,14 +5,19 @@ import numpy as np
 
 base_config = load_yaml('base.yaml')
 
-# decoder layers, decoder points, weight decay, filter protons, positional noise, dropout, embedding_dim, bottleneck_dim, num_convs, num_nodewise
+# decoder layers, decoder points, weight decay, filter protons, positional noise, dropout, embedding_dim, bottleneck_dim, num_convs, num_nodewise, ramp_depth
 config_list = [
-    [4, 256, 0.05, True, 0, 0, 256, 256, 1, 4],  # 0
-    [4, 256, 0.05, False, 0, 0, 256, 256, 1, 4],  # 1
-    [4, 256, 0.05, True, 0, 0, 256, 256, 0, 4],  # 2
-    [4, 256, 0.05, False, 0, 0, 256, 256, 0, 4],  # 3
-    [4, 256, 0.05, True, 0, 0, 256, 256, 2, 2],  # 4
-    [4, 256, 0.05, False, 0, 0, 256, 256, 2, 2],  # 5
+    [4, 256, 0.05, True, 0, 0, 256, 256, 1, 4, True],  # 0 - converged
+    [4, 256, 0.05, False, 0, 0, 256, 256, 1, 4, True],  # 1 - converged
+    [4, 256, 0.05, True, 0, 0, 256, 256, 0, 4, True],  # 2  cancelled - flat
+    [4, 256, 0.05, False, 0, 0, 256, 256, 0, 4, True],  # 3  cancelled - flat
+    [4, 256, 0.05, True, 0, 0, 256, 256, 2, 2, True],  # 4 - converged - ~worse than 0
+    [4, 256, 0.05, False, 0, 0, 256, 256, 2, 2, True],  # 5 - converged - ~worse than 1
+    [4, 256, 0.1, True, 0, 0, 256, 256, 1, 4, True],  # 6 - converged
+    [8, 256, 0.05, True, 0, 0, 256, 256, 1, 4, True],  # 7 - converged
+    [4, 256, 0.05, True, 0, 0, 512, 512, 1, 4, True],  # 8 - converged
+    [4, 256, 0.05, True, 0, 0, 256, 256, 1, 4, False],  # 9 - converged
+
 ]
 
 np.random.seed(1)
@@ -33,6 +38,7 @@ for ix1 in range(len(config_list)):
     config['autoencoder']['model']['bottleneck_dim'] = config_list[ix1][7]
     config['autoencoder']['model']['num_graph_convolutions'] = config_list[ix1][8]
     config['autoencoder']['model']['nodewise_fc_layers'] = config_list[ix1][9]
+    config['autoencoder']['model']['decoder_ramp_depth'] = config_list[ix1][10]
 
     with open(str(ind) + '.yaml', 'w') as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
