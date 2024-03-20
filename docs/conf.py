3ec6078588ed18818bfde84a4e91cea34d1a647e
InfluenceFunctional <mjakilgour@gmail.com>
2024-03-20 13:38:43 -0400

    :> new benchmark runs

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-from mxtaltoolx import __version__, __file__
 
 sys.path.insert(0, os.path.abspath("../"))
 
