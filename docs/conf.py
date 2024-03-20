f6346b60a7845abb85d77f447e822b229691b77e
InfluenceFunctional <mjakilgour@gmail.com>
2024-03-20 14:30:22 -0400

    :> new benchmark runs

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = "2024, Michael Kilgour"
 author = "Michael Kilgour"
 
 # The full version, including alpha/beta/rc tags
-version = "0.0.1"
+version = "0.1.0"
 release = "0.1"
 
 # -- General configuration ---------------------------------------------------
