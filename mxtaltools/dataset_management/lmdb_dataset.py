8ca7fde4583b5b2f916bcb6e54fa41678c182f88
InfluenceFunctional <mjakilgour@gmail.com>
2024-06-05 14:43:28 -0400

    :> new drugs processor

--- mxtaltools/dataset_management/lmdb_dataset.py
+++ mxtaltools/dataset_management/lmdb_dataset.py
@@ -45,7 +45,7 @@ class GeomDataset(Dataset):
         self.env = lmdb.open(
             self.path_to_datafiles,
             readonly=True,
-            max_readers=128,
+            max_readers=1,
         )
         self.txn = self.env.begin()
 
