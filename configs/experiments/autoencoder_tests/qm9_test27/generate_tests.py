af868179ca61da9c1d8192fe2db91a0cde8037c2
InfluenceFunctional <mjakilgour@gmail.com>
2024-10-05 15:03:53 -0400

    :> big architecture change

--- configs/experiments/autoencoder_tests/qm9_test27/generate_tests.py
+++ configs/experiments/autoencoder_tests/qm9_test27/generate_tests.py
@@ -836,6 +836,42 @@ config_list = [
                     'num_nodes': 512
                 }}}
     },  # 22 - new large baseline
+    {
+        'dataset': {'filter_protons': True},
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-4,
+                'min_lr': 1e-6,
+                'weight_decay': 0,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 362,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 362,
+                        'message_dim': 362//2,
+                        'embedding_dim': 362,
+                        'num_convs': 1,
+                        'fcs_per_gc': 1,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': None
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 362,
+                        'num_layers': 8,
+                        'dropout': 0,
+                        'norm': None},
+                    'num_nodes': 362
+                }}}
+    },  # 23 - slightly smaller big baseline
 
 ]
 
