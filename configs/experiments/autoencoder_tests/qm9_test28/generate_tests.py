7e55fdafe477635d4aed37b55ad720871acb5c75
InfluenceFunctional <mjakilgour@gmail.com>
2024-10-09 18:15:22 -0400

    :> proxy discriminator development and configs

--- configs/experiments/autoencoder_tests/qm9_test28/generate_tests.py
+++ configs/experiments/autoencoder_tests/qm9_test28/generate_tests.py
@@ -587,6 +587,310 @@ config_list = [
                     'num_nodes': 512
                 }}}
     },  # 14 - baseline with higher threshold
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.15,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 15 - baseline with higher threshold
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.2,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 16 - baseline with higher threshold
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.25,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 17 - baseline with higher threshold
+    {
+        'positional_noise': {'autoencoder': 0.25},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.1,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 18 - playing with 14
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.1,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 256,
+                        'message_dim': 128,
+                        'embedding_dim': 256,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 256,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 256
+                }}}
+    },  # 18 - playing with 14
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.1,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 1,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 19 - playing with 14
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.1,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 256,
+                        'num_layers': 8,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 20 - playing with 14
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'nearest_node_loss_coefficient': 0.1,
+            'clumping_loss_coefficient': 0.1,
+            'sigma_threshold': 0.1,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.99,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': None,
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': None},
+                    'num_nodes': 512
+                }}}
+    },  # 21 - playing with 14
 ]
 
 
