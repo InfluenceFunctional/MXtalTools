dd8e3ea63a0a298f53418c314294892fffd09154
InfluenceFunctional <mjakilgour@gmail.com>
2024-10-15 11:36:39 -0400

    :> proxy discriminator development and configs

--- configs/experiments/autoencoder_tests/qm9_test28/generate_tests.py
+++ configs/experiments/autoencoder_tests/qm9_test28/generate_tests.py
@@ -42,7 +42,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 0 - baseline w new losses
+    },  # 0 - baseline w new losses - decent, <0.2, 2-5th crashed, 6th run tied for best at .12
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -81,7 +81,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 1 - baseline w new losses
+    },  # 1 - baseline w new losses - quite bad, terrible overfit ~ 0.3
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -120,7 +120,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 2 - baseline w new losses
+    },  # 2 - baseline w new losses - mediocre ~ 0.2
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -159,7 +159,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 3 - baseline w new losses
+    },  # 3 - baseline w new losses - crashed early
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -198,7 +198,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 4 - baseline w new losses
+    },  # 4 - baseline w new losses - crashed early
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -237,7 +237,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 5 - baseline w new losses
+    },  # 5 - baseline w new losses - very slow convergence, still improving at (terrible) finish
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -276,7 +276,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 0 - baseline w new losses
+    },  # 6 - baseline w new losses - slow and high. Second run crashed
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -315,7 +315,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 1 - baseline w new losses
+    },  # 7 - baseline w new losses - mediocre .25
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -354,7 +354,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 2 - baseline w new losses
+    },  # 8 - baseline w new losses - mediocre ~ 0.3
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -393,7 +393,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 3 - baseline w new losses
+    },  # 9 - baseline w new losses - mediocre ~ 03
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -432,7 +432,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 4 - baseline w new losses
+    },  # 10 - baseline w new losses - quite poor, barely hits ~0.3
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -471,7 +471,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 5 - baseline w new losses
+    },  # 11 - baseline w new losses - very high loss and not improving
     {
         
         'positional_noise': {'autoencoder': 0},
@@ -510,7 +510,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 128
                 }}}
-    },  # 12 - small output speed test
+    },  # 12 - small output speed test - crashed early
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -548,7 +548,7 @@ config_list = [
                         'norm': None},
                     'num_nodes': 256
                 }}}
-    },  # 13 - faster run
+    },  # 13 - faster run - hard flatline above 0.2
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -586,7 +586,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 14 - baseline with higher threshold
+    },  # 14 - baseline with higher threshold - tied for best at 0.12
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -624,7 +624,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 15 - baseline with higher threshold
+    },  # 15 - baseline with higher threshold - tied for best at 0.12
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -662,7 +662,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 16 - baseline with higher threshold
+    },  # 16 - baseline with higher threshold - flatline around 0.2
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -700,7 +700,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 17 - baseline with higher threshold
+    },  # 17 - baseline with higher threshold - tied for best >0.12
     {
         'positional_noise': {'autoencoder': 0.25},
         'autoencoder': {
@@ -738,7 +738,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 18 - playing with 14
+    },  # 18 - playing with 14 - very fast and severe overfit - very odd. a little positional noise makes it really weird
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -776,7 +776,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 256
                 }}}
-    },  # 18 - playing with 14
+    },  # 19 - playing with 14 - flat about 0.2
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -814,7 +814,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 19 - playing with 14
+    },  # 20 - playing with 14 - interesting. flatline with Minimal overfit, not quite SOTA though
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -852,7 +852,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 20 - playing with 14
+    },  # 21 - playing with 14 - crashed early
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -890,7 +890,7 @@ config_list = [
                         'norm': None},
                     'num_nodes': 512
                 }}}
-    },  # 21 - playing with 14
+    },  # 22 - playing with 14 - bit worse than SOTA
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -926,7 +926,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 22 - oct11 - new series
+    },  # 23 - oct11 - new series - SOTA and still improving after 2 days, starting to overfit
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -962,7 +962,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 1024
                 }}}
-    },  # 23 - oct11 - new series
+    },  # 24 - oct11 - new series - crashed right away
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -998,7 +998,7 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 24 - oct11 - new series
+    },  # 25 - oct11 - new series - minimal overfit, SOTA, still improving
     {
         'positional_noise': {'autoencoder': 0},
         'autoencoder': {
@@ -1034,7 +1034,153 @@ config_list = [
                         'norm': 'layer'},
                     'num_nodes': 512
                 }}}
-    },  # 25 - oct11 - new series
+    },  # 26 - oct11 - new series - not SOTA but test loss loglog still improving
+    # next convergence series - 7 day runs
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'sigma_threshold': 0.15,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.995,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 27 - no dropout baseline
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'sigma_threshold': 0.15,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.995,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0.25,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 28 - decoder dropout
+    {
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'sigma_threshold': 0.15,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.995,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0.25,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0.25,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 29 double dropout
+    {
+        'positional_noise': {'autoencoder': 0.05},
+        'autoencoder': {
+            'sigma_threshold': 0.15,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 1e-3,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.995,
+            },
+            'model': {
+                'bottleneck_dim': 512,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 128,
+                        'embedding_dim': 512,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': 'graph layer'
+                    }},
+                'decoder': {
+                    'fc': {
+                        'hidden_dim': 512,
+                        'num_layers': 4,
+                        'dropout': 0.25,
+                        'norm': 'layer'},
+                    'num_nodes': 512
+                }}}
+    },  # 30 - decocer dropout and a little noise
+
 ]
 
 
