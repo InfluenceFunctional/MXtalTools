e669052e61ddc263c852c7b8f7e7b183a8f611d8
InfluenceFunctional <mjakilgour@gmail.com>
2024-11-30 08:29:53 -0500

    :> on-the-fly smiles-to-conformers with random dihedrals :> more configs

--- configs/experiments/autoencoder_tests/otf_zinc_test1/generate_tests.py
+++ configs/experiments/autoencoder_tests/otf_zinc_test1/generate_tests.py
@@ -50,7 +50,7 @@ config_list = [
                     },
                     'num_nodes': 64
                 }}}
-    },  # 0 - baseline mlp
+    },  # 0 - baseline mlp - excellent
     {
         'dataset': {'otf_build_size': 10000},
         'positional_noise': {'autoencoder': 0},
@@ -96,7 +96,7 @@ config_list = [
                     },
                     'num_nodes': 64
                 }}}
-    },  # 1 - baseline mlp bigger otf
+    },  # 1 - baseline mlp bigger otf - quite janky
     {
         'dataset': {'otf_build_size': 1000},
         'positional_noise': {'autoencoder': 0.1},
@@ -142,7 +142,7 @@ config_list = [
                     },
                     'num_nodes': 64
                 }}}
-    },  # 2 - baseline mlp with noise & rescaling
+    },  # 2 - baseline mlp with noise & rescaling - slower, indeed quite slow?
     {
         'dataset': {'otf_build_size': 10000},
         'positional_noise': {'autoencoder': 0.1},
@@ -188,9 +188,54 @@ config_list = [
                     },
                     'num_nodes': 64
                 }}}
-    },  # 3 - big mlp with noise and big otf
-
-
+    },  # 3 - big mlp with noise and big otf - crashed
+    {
+        'dataset': {'otf_build_size': 1000},
+        'positional_noise': {'autoencoder': 0},
+        'autoencoder': {
+            'overlap_eps': {'test': 1e-4},
+            'affine_scale_factor': None,
+            'filter_protons': False,
+            'infer_protons': False,
+            'sigma_threshold': 0.15,
+            'nearest_node_loss_coefficient': 0.01,
+            'clumping_loss_coefficient': 0.01,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 5e-4,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.9975,
+            },
+            'model': {
+                'bottleneck_dim': 256,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 256,
+                        'message_dim': 64,
+                        'embedding_dim': 256,
+                        'num_convs': 2,
+                        'fcs_per_gc': 2,
+                        'dropout': 0,
+                        'cutoff': 3,
+                        'norm': None,
+                        'vector_norm': None,
+                    }},
+                'decoder': {
+                    'model_type': 'mlp',
+                    'fc': {
+                        'hidden_dim': 256,
+                        'num_layers': 4,
+                        'dropout': 0,
+                        'norm': None,
+                        'vector_norm': None,
+                    },
+                    'num_nodes': 64
+                }}}
+    },  # 4 - baseline mlp with lower sigma converge
 
 ]
 
