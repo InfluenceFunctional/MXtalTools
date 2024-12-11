856ac55d9fa8d9a9f7ae041c420927e68747370e
InfluenceFunctional <mjakilgour@gmail.com>
2024-12-11 14:36:55 -0500

    :> regressor benchmark

--- configs/experiments/autoencoder_tests/otf_zinc_test4/generate_tests.py
+++ configs/experiments/autoencoder_tests/otf_zinc_test4/generate_tests.py
@@ -532,7 +532,103 @@ config_list = [
                     },
                     'num_nodes': 64
                 }}}
-    },  # 10 - very large GNN with norming and noise
+    },  # 10 - very large GNN with norming, dropout, and noise
+    {
+        'dataset': {'otf_build_size': 10000},
+        'positional_noise': {'autoencoder': 0.1},
+        'autoencoder': {
+            'affine_scale_factor': 1,
+            'filter_protons': False,
+            'infer_protons': False,
+            'sigma_threshold': 0.15,
+            'init_sigma': 2,
+            'nearest_node_loss_coefficient': 0.01,
+            'clumping_loss_coefficient': 0.01,
+            'nearest_component_loss_coefficient': 0.1,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 5e-4,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.9985,
+            },
+            'model': {
+                'bottleneck_dim': 64,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 64,
+                        'embedding_dim': 512,
+                        'num_convs': 4,
+                        'fcs_per_gc': 2,
+                        'dropout': 0.25,
+                        'cutoff': 3,
+                        'norm': 'graph layer',
+                        'vector_norm': 'graph vector layer',
+                    }},
+                'decoder': {
+                    'model_type': 'gnn',
+                    'fc': {
+                        'hidden_dim': 64,
+                        'num_layers': 8,
+                        'dropout': 0.25,
+                        'norm': 'layer',
+                        'vector_norm': 'vector layer',
+                    },
+                    'num_nodes': 64
+                }}}
+    },  # 11 - very large GNN with norming, dropout, and noise, and 64 bottleneck
+    {
+        'dataset': {'otf_build_size': 10000},
+        'positional_noise': {'autoencoder': 0.1},
+        'autoencoder': {
+            'affine_scale_factor': 1,
+            'filter_protons': False,
+            'infer_protons': False,
+            'sigma_threshold': 0.15,
+            'init_sigma': 2,
+            'nearest_node_loss_coefficient': 0.01,
+            'clumping_loss_coefficient': 0.01,
+            'nearest_component_loss_coefficient': 0.1,
+            'optimizer': {
+                'init_lr': 5e-5,
+                'encoder_init_lr': 1e-4,
+                'decoder_init_lr': 1e-4,
+                'max_lr': 5e-4,
+                'min_lr': 1e-6,
+                'weight_decay': 0.05,
+                'lr_growth_lambda': 1.05,
+                'lr_shrink_lambda': 0.9985,
+            },
+            'model': {
+                'bottleneck_dim': 128,
+                'encoder': {
+                    'graph': {
+                        'node_dim': 512,
+                        'message_dim': 64,
+                        'embedding_dim': 512,
+                        'num_convs': 4,
+                        'fcs_per_gc': 2,
+                        'dropout': 0.25,
+                        'cutoff': 3,
+                        'norm': 'graph layer',
+                        'vector_norm': 'graph vector layer',
+                    }},
+                'decoder': {
+                    'model_type': 'gnn',
+                    'fc': {
+                        'hidden_dim': 64,
+                        'num_layers': 8,
+                        'dropout': 0.25,
+                        'norm': 'layer',
+                        'vector_norm': 'vector layer',
+                    },
+                    'num_nodes': 64
+                }}}
+    },  # 11 - very large GNN with norming, dropout, and noise, and 128 bottleneck
 
 ]
 
