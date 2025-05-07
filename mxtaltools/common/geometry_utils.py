:> fix for gradient descent cell optimization

mxtaltools/common/geometry_utils.py
@@ -206,7 +206,7 @@ def list_molecule_principal_axes_torch(coords_list: list = None,
     return Ip_fin, Ipm_fin, I
 
 
-def batch_molecule_principal_axes_torch(coords: torch.FloatTensor,
+def batch_molecule_principal_axes_torch(coords_i: torch.FloatTensor,
                                         batch: torch.LongTensor,
                                         num_graphs: int,
                                         nodes_per_graph: torch.LongTensor,
@@ -227,7 +227,9 @@ def batch_molecule_principal_axes_torch(coords: torch.FloatTensor,
     """
 
     if not skip_centring:
-        coords = center_mol_batch(coords, batch, num_graphs, nodes_per_graph)
+        coords = center_mol_batch(coords_i, batch, num_graphs, nodes_per_graph)
+    else:
+        coords = coords_i
 
     Ip, Ipm_fin, I = scatter_compute_Ip(coords, batch)
 
@@ -235,8 +237,7 @@ def batch_molecule_principal_axes_torch(coords: torch.FloatTensor,
     direction = batch_get_furthest_node_vector(coords, batch, num_graphs)
     normed_direction = direction / torch.linalg.norm(direction, dim=1)[:, None]
     overlaps, signs = get_overlaps(Ip, normed_direction)
-    Ip_fin = correct_Ip_directions(Ip, overlaps,
-                                   signs)  # somehow, fails for mirror planes, on top of symmetric and spherical tops
+    Ip_fin = correct_Ip_directions(Ip, overlaps, signs)  # fails for mirror planes, on top of symmetric and spherical tops
 
     return Ip_fin, Ipm_fin, I
 
@@ -276,10 +277,10 @@ def correct_Ip_directions(Ip, overlaps, signs, overlap_threshold: float = 1e-5):
     Ip_fin: torch.tensor(3,3)
         Inertial principal axes with positive overlaps to the given canonical direction
     """
-    Ip_fin = torch.zeros_like(Ip)
+    Ip_fixed_list = []
     for ii, Ip_i in enumerate(Ip):
         # if the vectors have negative overlap, flip the direction,
-        Ip_i = (Ip_i.T * signs[ii]).T
+        Ip_fixed = (Ip_i.T * signs[ii]).T
         # if any overlaps are vanishing (up to 32 bit precision),
         # happens if the cardinal direction is too close to an existing principal axis
         # determine the direction via the RHR (if two overlaps are vanishing, this will not work)
@@ -287,9 +288,10 @@ def correct_Ip_directions(Ip, overlaps, signs, overlap_threshold: float = 1e-5):
             # enforce right-handedness in the free vector (vanishing overlap)
             fix_ind = torch.argmin(torch.abs(overlaps[ii]))  # vector with vanishing overlap
             if compute_Ip_handedness(Ip_i) < 0:  # if result is not right-handed, swap it
-                Ip_i[fix_ind] = -Ip_i[fix_ind]
+                Ip_fixed[fix_ind] = -Ip_fixed[fix_ind]
 
-        Ip_fin[ii] = Ip_i
+        Ip_fixed_list.append(Ip_fixed)
+    Ip_fin = torch.stack(Ip_fixed_list)
     return Ip_fin
 
 
@@ -369,14 +371,14 @@ def scatter_compute_Ip(all_coords, batch):
          torch.vstack((Ixz, Iyz, Izz))[:, None, :].permute(2, 1, 0)
          ), dim=-2)  # inertial tensor
 
-    Ipm, Ip = torch.linalg.eig(inertial_tensor)  # principal inertial tensor
-    Ipm, Ip = torch.real(Ipm), torch.real(Ip)
+    Ipm_c, Ip_c = torch.linalg.eig(inertial_tensor)  # principal inertial tensor
+    Ipms, Ip_o = torch.real(Ipm_c), torch.real(Ip_c)
 
-    Ip = Ip.permute(0, 2, 1)  # switch to row-wise eigenvectors
+    Ips = Ip_o.permute(0, 2, 1)  # switch to row-wise eigenvectors
 
-    sort_inds = torch.argsort(Ipm, dim=1)
-    Ipm = torch.stack([Ipm[i, sort_inds[i]] for i in range(len(sort_inds))])
-    Ip = torch.stack([Ip[i][sort_inds[i]] for i in range(len(sort_inds))])  # sort also the eigenvectors
+    sort_inds = torch.argsort(Ipms, dim=1)
+    Ipm = torch.stack([Ipms[i, sort_inds[i]] for i in range(len(sort_inds))])
+    Ip = torch.stack([Ips[i][sort_inds[i]] for i in range(len(sort_inds))])  # sort also the eigenvectors
 
     return Ip, Ipm, inertial_tensor
 
@@ -1259,8 +1261,8 @@ def center_mol_batch(coords: torch.FloatTensor,
                      nodes_per_graph: torch.LongTensor,
                      ) -> Tensor:
     centroids = get_batch_centroids(coords, batch, num_graphs)
-    coords -= centroids.repeat_interleave(nodes_per_graph, 0)
-    return coords
+    coords_out = coords - centroids.repeat_interleave(nodes_per_graph, 0)
+    return coords_out
 
 
 def batch_compute_mol_mass(z: torch.LongTensor,

mxtaltools/crystal_search/standalone_crystal_opt.py
@@ -14,7 +14,7 @@ from mxtaltools.models.utils import enforce_1d_bound, softmax_and_score
 
 def standalone_gradient_descent_optimization(
         init_sample: torch.Tensor,
-        crystal_batch,
+        init_crystal_batch,
         optimizer_func: Optional = torch.optim.Rprop,
         convergence_eps: Optional[float] = 1e-3,
         lr: Optional[float] = 1e-4,
@@ -41,18 +41,20 @@ def standalone_gradient_descent_optimization(
     scheduler1 = optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lambda epoch: 0.985)
     scheduler2 = optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lambda epoch: grow_lambda)
     hit_max_lr = False
-    loss_record = torch.zeros((max_num_steps, crystal_batch.num_graphs))
+    loss_record = torch.zeros((max_num_steps, init_crystal_batch.num_graphs))
     lr_record = torch.zeros(max_num_steps)
-    params_record = torch.zeros((max_num_steps, crystal_batch.num_graphs, 12), dtype=torch.float32)
+    params_record = torch.zeros((max_num_steps, init_crystal_batch.num_graphs, 12), dtype=torch.float32)
 
     optimization_trajectory = []
 
     converged = False
+    #torch.autograd.set_detect_anomaly(True)
     with (torch.enable_grad()):
         with tqdm(total=max_num_steps, disable=not show_tqdm) as pbar:
             s_ind = 0
             while not converged:
                 optimizer.zero_grad()
+                crystal_batch = init_crystal_batch.clone().detach()
                 crystal_batch.set_cell_parameters(params_to_optim)
                 crystal_batch.clean_cell_parameters()
                 lj_pot, es_pot, scaled_lj_pot, cluster_batch = crystal_batch.build_and_analyze(return_cluster=True,
@@ -77,17 +79,17 @@ def standalone_gradient_descent_optimization(
                 "auxiliary losses"
                 if target_packing_coeff is not None:
                     cp_loss = (cluster_batch.packing_coeff - target_packing_coeff) ** 2
-                    loss += cp_loss
+                    loss = loss + cp_loss
 
                 if do_box_restriction:
                     # enforce box shape cannot become too long (3 mol radii) or narrow (3 angstroms) in any direction
                     aunit_lengths = cluster_batch.scale_lengths_to_aunit()
                     box_loss = (80000 / aunit_lengths ** 12).sum(
                         dim=1)  # repulsive from about range 3 #(80000/aunit_lengths**12 + 10*aunit_lengths - 31.25).sum(dim=1)  # forces boxes to be larger than 3 angstroms, but squeezes them otherwise
-                    loss += box_loss
+                    loss = loss + box_loss
 
                 if compression_factor != 0:
-                    loss += aunit_lengths.sum(dim=1) * compression_factor
+                    loss = loss + aunit_lengths.sum(dim=1) * compression_factor
 
                 loss.mean().backward()  # compute gradients
                 torch.nn.utils.clip_grad_norm_(params_to_optim, grad_norm_clip)  # gradient clipping
@@ -136,13 +138,24 @@ lj_pots = torch.stack(
     [torch.tensor([sample.scaled_lj_pot for sample in sample_list]) for sample_list in optimization_trajectory])
 coeffs = torch.stack(
     [torch.tensor([sample.packing_coeff for sample in sample_list]) for sample_list in optimization_trajectory])
-    
+params = torch.stack([torch.cat([sample.cell_parameters() for sample in sample_list]) for sample_list in optimization_trajectory])
+
 fig = go.Figure()
 fig.add_scatter(x=coeffs[0,:], y=lj_pots[0, :], mode='markers', marker_size=20, marker_color='grey', name='Initial State')
 fig.add_scatter(x=coeffs[-1,:], y=lj_pots[-1, :], mode='markers', marker_size=20, marker_color='black', name='Final State')
 for ind in range(coeffs.shape[1]):
     fig.add_scatter(x=coeffs[:, ind], y=lj_pots[:, ind], name=f"Run {ind}")
 fig.update_layout(xaxis_title='Packing Coeff', yaxis_title='Scaled LJ')
+fig.show()
+
+
+from plotly.subplots import make_subplots
+fig = make_subplots(rows=4, cols=3, subplot_titles = ['a','b','c','al','be','ga','x','y','z','u','v','w'])
+for ind in range(12):
+    row = ind // 3 + 1
+    col = ind % 3 + 1
+    for ind2 in range(params.shape[1]):
+        fig.add_scatter(y=params[:, ind2, ind], showlegend=False, row=row, col=col)
 fig.show()
 
 # import plotly.graph_objects as go
@@ -161,6 +174,10 @@ fig.show()
 #                     showlegend=True if ind == 0 else False)
 # fig.update_layout(yaxis_range=[0,1])
 # fig.show()
+
+fig = make_subplots(rows=4, cols=3)
+for ind in range(12):
+
 
 lj_pot, es_pot, scaled_lj_pot, cluster_batch = crystal_batch.build_and_analyze(return_cluster=True)
 cluster_batch.visualize(mode='convolve with')