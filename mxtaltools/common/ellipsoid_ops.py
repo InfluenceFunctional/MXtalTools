:> fixed gradient flow in new generator / ellipsoid :> added grade 3 to curriculum :> small optimizations

mxtaltools/common/ellipsoid_ops.py
@@ -214,11 +214,12 @@ def featurize_ellipsoid_batch(Ip, edge_i_good, edge_j_good, mol_centroids, semi_
     # relative rotation matrix
     # R_rel = torch.einsum('nik, njk -> nij', unit_std_normed_e1, unit_std_normed_e2)
     cmat = compute_cosine_similarity_matrix(unit_std_normed_e1, unit_std_normed_e2)
-    x = torch.zeros((len(e1), 22), dtype=torch.float32, device=device)
-    x[:, 0] = normed_r.norm(dim=-1)
-    x[:, 1:10] = cmat.reshape(len(e1), 9)
-    x[:, 10:13] = std_normed_e1.norm(dim=-1)
-    x[:, 13:16] = std_normed_e2.norm(dim=-1)
-    x[:, 16:19] = r1_local
-    x[:, 19:22] = r2_local
+    x = torch.cat([
+        normed_r.norm(dim=-1, keepdim=True),
+        cmat.reshape(len(e1), 9),
+        std_normed_e1.norm(dim=-1),
+        std_normed_e2.norm(dim=-1),
+        r1_local,
+        r2_local,
+    ], dim=1)
     return max_val, normed_v1, normed_v2, x

mxtaltools/common/geometry_utils.py
@@ -371,7 +371,7 @@ def scatter_compute_Ip(all_coords, batch):
          torch.vstack((Ixz, Iyz, Izz))[:, None, :].permute(2, 1, 0)
          ), dim=-2)  # inertial tensor
 
-    Ipm_c, Ip_c = torch.linalg.eig(inertial_tensor)  # principal inertial tensor
+    Ipm_c, Ip_c = torch.linalg.eigh(inertial_tensor)  # principal inertial tensor # eigh faster on hermit matrices
     Ipms, Ip_o = torch.real(Ipm_c), torch.real(Ip_c)
 
     Ips = Ip_o.permute(0, 2, 1)  # switch to row-wise eigenvectors

mxtaltools/dataset_utils/data_classes.py
@@ -1175,8 +1175,9 @@ class MolCrystalData(MolData):
             r=self.radius.amax() * 2)
         # filter edges longer than 2x mol_radius for each sample
         dists = torch.linalg.norm(mol_centroids[edge_i] - mol_centroids[edge_j], dim=1)
-        edge_i_good = edge_i[dists < (2 * longest_length[edge_i])]
-        edge_j_good = edge_j[dists < (2 * longest_length[edge_i])]
+        good_inds = dists < (2 * longest_length[edge_i])
+        edge_i_good = edge_i[good_inds]
+        edge_j_good = edge_j[good_inds]
         return edge_i_good, edge_j_good, mol_centroids
 
     def compute_cluster_ellipsoids(self, semi_axis_scale):

mxtaltools/modeller.py
@@ -2039,6 +2039,13 @@ class Modeller:
         self.stage2_max_ellipsoid_scale = 0.75
         self.stage2_max_orientation_scale = 1
 
+        self.stage3_ell_cutoff = 0.1
+        self.stage3_density_cutoff = 0.4
+        self.stage3_max_length_scale = 1
+        self.stage3_max_angle_scale = 1
+        self.stage3_max_density_loss_coefficient = 1
+        self.stage3_max_ellipsoid_scale = 1
+
         stats = {'ellipsoid_scale': self.ellipsoid_scale,
                  'vdw_turnover_pot': self.vdw_turnover_pot,
                  'density_loss_scale': self.density_loss_scale,
@@ -2068,6 +2075,8 @@ class Modeller:
 
         ellipsoid_losses = self.logger.train_stats['ellipsoid_loss']
         trailing_ellipsoid_loss = np.mean(ellipsoid_losses[-self.curriculum_history:])
+        density_losses = self.logger.train_stats['density_loss']
+        trailing_density_loss = np.mean(density_losses[-self.curriculum_history:])
 
         """annealing checks"""
         if self.learning_stage == 1:
@@ -2080,13 +2089,17 @@ class Modeller:
         elif self.learning_stage == 2:
             """allow orientation changes and larger ellipsoids"""
             if trailing_ellipsoid_loss < self.stage2_cutoff:
-                if self.ellipsoid_scale < self.stage2_max_ellipsoid_scale:
-                    if self.orientation_scale < self.stage2_max_orientation_scale:
-                        self.ellipsoid_scale = min(self.ellipsoid_scale * self.increment_fraction, self.stage2_max_ellipsoid_scale)
-                        self.orientation_scale = min(self.orientation_scale * self.increment_fraction, self.stage2_max_orientation_scale)
+                    self.ellipsoid_scale = min(self.ellipsoid_scale * self.increment_fraction, self.stage2_max_ellipsoid_scale)
+                    self.orientation_scale = min(self.orientation_scale * self.increment_fraction, self.stage2_max_orientation_scale)
 
         elif self.learning_stage == 3:
-            aa = 1
+            if trailing_ellipsoid_loss < self.stage3_ell_cutoff:
+                if trailing_density_loss < self.stage3_density_cutoff:
+                    self.length_scale = min(self.length_scale * self.increment_fraction, self.stage3_max_length_scale)
+                    self.angle_scale = min(self.angle_scale * self.increment_fraction, self.stage3_max_angle_scale)
+                    self.density_loss_coefficient = min(self.density_loss_coefficient * self.increment_fraction, self.stage3_max_density_loss_coefficient)
+                    self.ellipsoid_scale = min(self.ellipsoid_scale * self.increment_fraction, self.stage3_max_ellipsoid_scale)
+
 
         """curriculum updates"""
         if self.learning_stage == 1:
@@ -2486,7 +2499,7 @@ class Modeller:
 
             # analyze cell
             (molwise_normed_overlap, molwise_scaled_lj_pot, molwise_lj_pot,
-             ellipsoid_overlap, silu_pot, cluster_batch) = self.analyze_generator_samples(crystal_batch)
+             ellipsoid_overlap, silu_pot, cluster_batch) = self.analyze_generator_samples(new_crystal_batch)
             vdw_loss, density_loss, ellipsoid_loss = self.generator_losses(cluster_batch, silu_pot, ellipsoid_overlap)
 
             loss = (self.ellipsoid_loss_coefficient * ellipsoid_loss +