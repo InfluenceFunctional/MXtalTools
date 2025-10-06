c9b7dc1e122cd38b774bf681e9f15e30f1696508
InfluenceFunctional <mjakilgour@gmail.com>
2025-10-06 16:18:57 -0400

    :> search configs

---
+++ configs/crystal_searches/qm9/search1/generate_configs.py
@@ -0,0 +1,21 @@
+from copy import copy
+
+import yaml
+
+from mxtaltools.common.config_processing import load_yaml
+
+base_config = load_yaml('base.yaml')
+
+num_configs = 100
+
+"""
+sequentially numbered sweep configs
+"""
+for ix1 in range(num_configs):
+    config = copy(base_config)
+    config['run_name'] = f"run_{ix1}"
+    config['opt_seed'] = ix1
+    config['mol_seed'] = ix1
+
+    with open(str(ix1) + '.yaml', 'w') as outfile:
+        yaml.dump(config, outfile, default_flow_style=False)
