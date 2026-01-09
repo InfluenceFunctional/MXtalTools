:> z'>1 edge construction fix :> explicit jacobian in rotational latent space :> new configs :> speedups

mxtaltools/common/geometry_utils.py
@@ -1532,7 +1532,8 @@ def fractional_transform(coords, transform_matrix):
         return fractional_transform_np(coords, transform_matrix)
     elif torch.is_tensor(coords):
         return fractional_transform_torch(coords, transform_matrix)
-
+    else:
+        assert False
 
 def fractional_transform_np(coords, transform_matrix):
     if coords.ndim == 2 and transform_matrix.ndim == 2:
@@ -1711,3 +1712,23 @@ def crystal_parameter_distmat(
         distmat[i:i + b] = d.view(b, N)
 
     return distmat
+
+
+def sph_rotvec2lat(sph_rotvec, z_prime):
+    sph = sph_rotvec.view(*sph_rotvec.shape[:-1], z_prime, 3)
+
+    # allocate output
+    lat = torch.empty_like(sph)
+
+    halfpi = torch.pi / 2
+
+    # theta: [0, π/2] → [-1, 1]
+    lat[..., 0] = (sph[..., 0] - (halfpi / 2)) / (torch.pi / 4)
+
+    # phi: [-π, π] → [-1, 1]
+    lat[..., 1] = sph[..., 1] / torch.pi
+
+    # r: [0, 2π] → [-1, 1]
+    lat[..., 2] = (sph[..., 2] - torch.pi) / torch.pi
+
+    return lat.view(*sph_rotvec.shape)

mxtaltools/crystal_building/utils.py
@@ -1,4 +1,5 @@
 import warnings
+from typing import Union
 
 import numpy as np
 import torch
@@ -26,7 +27,8 @@ def generate_sorted_fractional_translations(supercell_size):
 
 
 def ucell2cluster(crystal_batch, cutoff: float = 6,
-                  supercell_size: int = 10):  # todo this could be modularized/simplified
+                  supercell_size: int = 10,
+                  zp_buffer: Union[float, torch.Tensor] = 0):  # todo this could be modularized/simplified
     """
     1) generate supercell cluster including only unit cells which could plausibly interact with the asymmetric unit
     2) instantiate the cluster
@@ -41,7 +43,7 @@ def ucell2cluster(crystal_batch, cutoff: float = 6,
     good_translations, ucells_per_cluster = get_cart_translations(cc_centroids,
                                                                   crystal_batch.T_fc,
                                                                   crystal_batch.radius,
-                                                                  cutoff,
+                                                                  cutoff + zp_buffer,
                                                                   supercell_size)
 
     """
@@ -61,7 +63,6 @@ def ucell2cluster(crystal_batch, cutoff: float = 6,
     """
     Instantiate explicit clusters
     """
-
     cluster_batch = instantiate_cluster(atoms_per_ucell,
                                         atoms_per_cluster,
                                         atomwise_translation,
@@ -74,7 +75,7 @@ def ucell2cluster(crystal_batch, cutoff: float = 6,
     """
     cluster_batch = _pare_cluster_molwise(atoms_per_cluster, cc_centroids,
                                           cluster_batch, crystal_batch,
-                                          cutoff, aunits_per_cluster)
+                                          cutoff + zp_buffer, aunits_per_cluster)
 
     return cluster_batch
     # zpg_inds = torch.argwhere(cluster_batch.z_prime > 1).flatten().tolist()
@@ -83,7 +84,8 @@ def ucell2cluster(crystal_batch, cutoff: float = 6,
     # cluster_batch.visualize([1, 2, 3, 4, 5], mode='convolve with')
 
 
-def _pare_cluster_molwise(atoms_per_cluster, cc_centroids, cluster_batch, crystal_batch, cutoff, aunits_per_cluster):
+def _pare_cluster_molwise(atoms_per_cluster, cc_centroids, cluster_batch,
+                          crystal_batch, cutoff, aunits_per_cluster):
     # get all mol centroids
     # indexes which molecule each atom belongs to, globally (not within-crystal)
     aunit_ptr = torch.cat([torch.zeros(1, device=cluster_batch.device, dtype=torch.long),
@@ -346,14 +348,15 @@ def get_cart_translations(cc_centroids,
                           T_fc,
                           mol_radii,
                           cutoff,
-                          supercell_size: int = 9):
+                          supercell_size: int = 9,
+                          ):
     # precompute some stuff
     # get the set of all possible cartesian translations
     cell_vectors = T_fc.permute(0, 2, 1)
     # cell diagonals
     cell_diag = cell_vectors.sum(1).norm(dim=-1)
     # bounding box cutoff
-    cutoff_distance = mol_radii * 2 + cutoff + 0.1 + cell_diag  # convolutional radius plus cell diagonal
+    cutoff_distance = mol_radii * 2 + cutoff + 0.1 + cell_diag  # convolutional radius plus cell diagonal plus z'>1 buffer
 
     # identify actual supercell size
     box_lengths = torch.linalg.norm(cell_vectors, dim=-1)
@@ -457,7 +460,7 @@ def extract_aunit_orientation(mol_batch,
         mol_batch.num_graphs,
         mol_batch.num_atoms,
         heavy_atoms_only=True,
-        atom_types = mol_batch.z
+        atom_types=mol_batch.z
     )
 
     handedness_list = compute_Ip_handedness(Ip).long()
@@ -738,6 +741,7 @@ def get_aunit_positions(mol_batch,
 
     return pos
 
+
 def canonicalize_aunit_order(batch):
     # canonical aunit ordering according to distance to the origin, with x, y, z, tiebreak
     # TODO implement tiebreak
@@ -756,7 +760,7 @@ def canonicalize_aunit_order(batch):
 
     # also reorder the orientations accordingly
     per_aunit_orient = batch.aunit_orientation.reshape(batch.num_graphs, batch.max_z_prime, 3)
-    per_aunit_orient[mask] = torch.pi/2  # this places all unused samples in the same state
+    per_aunit_orient[mask] = torch.pi / 2  # this places all unused samples in the same state
     canonical_order_exp = canonical_order.unsqueeze(-1).expand(-1, -1, 3)  # [n, k, 3]
     sorted_orient = torch.gather(per_aunit_orient, dim=1, index=canonical_order_exp)
     aunit_orientation = sorted_orient.reshape(batch.num_graphs, batch.max_z_prime * 3)
@@ -808,4 +812,3 @@ def protonate_mol(atom_types, coords):
     view([m1, m2])
     
     """
-

mxtaltools/crystal_search/crystal_opt_utils.py
@@ -344,9 +344,10 @@ def compute_auxiliary_loss(cluster_batch, compression_factor, do_box_restriction
     return loss
 
 
-def inter_overlap_loss(cluster_batch, crystal_batch):
+def inter_overlap_loss(cluster_batch):
     # enforce molecules far enough away that they cannot possibly overlap
     # intermolecular centroid range repulsion
+    assert cluster_batch.z_prime.amax() == 1, "molwise business not implemented for Z'>1"
     _, atoms_per_cluster = torch.unique(cluster_batch.batch, return_counts=True)
     mols_per_cluster = (atoms_per_cluster / cluster_batch.num_atoms).long()
     molwise_batch = torch.arange(cluster_batch.num_graphs, device=cluster_batch.device).repeat_interleave(
@@ -365,7 +366,7 @@ def inter_overlap_loss(cluster_batch, crystal_batch):
     inter_dists = torch.linalg.norm(
         mol_centroids[edge_i] - mol_centroids[edge_j], dim=1
     )
-    scaled_inter_dists = inter_dists / crystal_batch.radius[molwise_batch[edge_i]]
+    scaled_inter_dists = inter_dists / cluster_batch.radius[molwise_batch[edge_i]]
     edgewise_losses = F.relu(-(scaled_inter_dists - 2))  # push molecules apart if they are within each others' radii
     loss = scatter(edgewise_losses,
                    molwise_batch[edge_i],

mxtaltools/dataset_utils/data_class_methods/crystal_building.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import torch
 
+from mxtaltools.common.geometry_utils import fractional_transform
 from mxtaltools.crystal_building.utils import get_aunit_positions, aunit2ucell, ucell2cluster
 from mxtaltools.dataset_utils.utils import collate_data_list
 
@@ -94,6 +95,9 @@ class MolCrystalBuilding:
                 torch.cumsum(atoms_per_cluster, dim=0)
             ])
 
+            cluster_batch.unit_cell_pos = zp1_batch.unit_cell_pos.clone()
+            cluster_batch.unit_cell_batch = batch_map[zp1_batch.unit_cell_batch].clone()
+
             return cluster_batch
 
         else:
@@ -120,13 +124,13 @@ class MolCrystalBuilding:
         else:
             self.unit_cell_pos, self.unit_cell_batch, self.unit_cell_mol_ind = aunit2ucell(collate_data_list([self]))
 
-    def build_cluster(self, cutoff: float = 6, supercell_size: int = 10):
+    def build_cluster(self, cutoff: float = 6, supercell_size: int = 10, zp_buffer=0):
         if self.is_batch:
-            return ucell2cluster(self, cutoff=cutoff, supercell_size=supercell_size)
+            return ucell2cluster(self, cutoff=cutoff, supercell_size=supercell_size, zp_buffer=zp_buffer)
         else:
             crystal_batch = collate_data_list([self])
             crystal_batch.build_unit_cell()
-            return ucell2cluster(crystal_batch, cutoff=cutoff, supercell_size=supercell_size)
+            return ucell2cluster(crystal_batch, cutoff=cutoff, supercell_size=supercell_size, zp_buffer=zp_buffer)
 
     def de_cluster(self):  # todo check and consider rewrite with new methods
         # delete cluster information and reset this object as a molecule
@@ -153,10 +157,17 @@ class MolCrystalBuilding:
         if self.max_z_prime > 1:
             # if there are any Z'>1 crystals in the batch, we
             # unzip for unit cell generation then re zip
+
+            # add the intra-aunit centroid distance to cutoffs
+            frac_centroids = self.aunit_centroid.reshape(self.num_graphs * self.max_z_prime, 3)
+            cart_centroids = fractional_transform(frac_centroids,self.T_fc.repeat_interleave(self.max_z_prime,dim=0)[1]).reshape(self.num_graphs, self.max_z_prime, 3)
+            dists = (cart_centroids[:, :, None, :] - cart_centroids[:, None, :, :]).norm(dim=-1) # [n, Zp, Zp, 3]
+            zp_buffer = dists.amax(dim=(1,2)).repeat_interleave(self.max_z_prime, dim=0)
+
             zp1_batch = self.split_to_zp1_batch()
             zp1_batch.pose_aunit(std_orientation=std_orientation)
             zp1_batch.build_unit_cell()
-            zp1_cluster = zp1_batch.build_cluster()
+            zp1_cluster = zp1_batch.build_cluster(cutoff=cutoff, supercell_size=supercell_size, zp_buffer=zp_buffer)
             return self.join_zp1_cluster_batch(zp1_cluster)
 
         else:

mxtaltools/dataset_utils/data_class_methods/crystal_ops.py
@@ -8,7 +8,7 @@ import torch
 from plotly.subplots import make_subplots
 
 from mxtaltools.common.geometry_utils import enforce_crystal_system, batch_compute_fractional_transform, \
-    cart2sph_rotvec, sph2cart_rotvec, crystal_parameter_distmat
+    cart2sph_rotvec, sph2cart_rotvec, crystal_parameter_distmat, sph_rotvec2lat, lat2sph_rotvec
 from mxtaltools.common.utils import softplus_shift, log_rescale_positive, get_point_density
 from mxtaltools.constants.asymmetric_units import ASYM_UNITS
 from mxtaltools.constants.space_group_info import SYM_OPS, LATTICE_TYPE
@@ -22,6 +22,9 @@ from mxtaltools.reporting.utils import lightweight_one_sided_violin
 
 
 # noinspection PyAttributeOutsideInit
+
+
+
 class MolCrystalOps:
 
     def assign_aunit_centroid(self, values, eps=1e-4):
@@ -311,18 +314,7 @@ class MolCrystalOps:
         # aunit centroid [0,1] to [-1,1]
         lat_centroids = (aunit_centroids - 0.5) * 2
 
-        # orientation vectors are individual, theta is [0, pi/2] to [-1,1]
-        theta_inds = [ind * 3 for ind in range(self.max_z_prime)]
-        phi_inds = [ind * 3 + 1 for ind in range(self.max_z_prime)]
-        r_inds = [ind * 3 + 2 for ind in range(self.max_z_prime)]
-
-        lat_orientations = torch.zeros_like(sph_rotvec)
-        # theta is [0, pi/2] to [-1, 1]
-        lat_orientations[:, theta_inds] = (sph_rotvec[:, theta_inds] - halfpi / 2) / torch.pi * 4
-        # phi is [-pi, pi] to [-1,1]
-        lat_orientations[:, phi_inds] = sph_rotvec[:, phi_inds] / torch.pi
-        # r is [0, 2pi] to [-1,1]
-        lat_orientations[:, r_inds] = (sph_rotvec[:, r_inds] - torch.pi) / torch.pi
+        lat_orientations = sph_rotvec2lat(sph_rotvec, self.max_z_prime)
 
         latents = torch.cat([lat_lengths, lat_angles, lat_centroids, lat_orientations], dim=1)
         return latents
@@ -340,18 +332,7 @@ class MolCrystalOps:
                                                                                          3 * self.max_z_prime], dim=1)
 
         '''reverse bounding transform'''
-        sph_rotvec = torch.zeros_like(lat_orientations)
-        theta_inds = [ind * 3 for ind in range(self.max_z_prime)]
-        phi_inds = [ind * 3 + 1 for ind in range(self.max_z_prime)]
-        r_inds = [ind * 3 + 2 for ind in range(self.max_z_prime)]
-        halfpi = torch.pi / 2
-
-        # phi is [0, pi/2] to [-1, 1]
-        sph_rotvec[:, theta_inds] = lat_orientations[:, theta_inds] * torch.pi / 4 + halfpi / 2
-        # phi is [-pi, pi] to [-1,1]
-        sph_rotvec[:, phi_inds] = lat_orientations[:, phi_inds] * torch.pi
-        # r is [0, 2pi] to [-1,1]
-        sph_rotvec[:, r_inds] = lat_orientations[:, r_inds] * torch.pi + torch.pi
+        sph_rotvec = lat2sph_rotvec(lat_orientations, self.max_z_prime)
 
         aunit_orientations = sph2cart_rotvec(sph_rotvec.reshape(len(sph_rotvec) * self.max_z_prime, 3)).reshape(
             len(sph_rotvec), self.max_z_prime * 3)