9c55d697063893096c71071e3636c0cda7326486
InfluenceFunctional <mjakilgour@gmail.com>
2026-05-11 17:54:46 -0400

    :> adam's MACE integration :> configs

--- configs/crystal_searches/acr_production/make_configs_acridine.py
+++ configs/crystal_searches/acr_production/make_configs_acridine.py
@@ -26,24 +26,38 @@ if __name__ == "__main__":
     base_path = 'acridine.yaml'
     base, spec_dir = load_yaml(base_path)
 
-    n_samples = 30000
+    n_samples = 40000
     n_parallel = 16
     bsz = 1000
-    ind = 40
-    for zp in [1]:
-        for sg in [14]:
-            for seed_ind in np.arange(ind, ind + n_parallel):
-                seed_ind = int(seed_ind)
-                config = deepcopy(base)
-                config['opt_seed'] = seed_ind
-                config['sgs_to_search'] = [sg]
-                config['zp_to_search'] = [zp]
-                config['run_name'] = f'may_acridine_sg{sg}_zp{zp}_{seed_ind}'
-                config['batch_size'] = bsz
-                config['num_samples'] = int(n_samples/n_parallel)
+    zps = []
+    sgs = []
+    zps.append(1) # Form II & XII
+    sgs.append(14)
+    zps.append(2) # Form III & VII
+    sgs.append(14)
+    zps.append(2) # Form VI
+    sgs.append(9)
+    zps.append(3) # Form IV
+    sgs.append(19)
 
-                config_path = f'{ind}.yaml'
-                with open(config_path, 'w') as f:
-                    yaml.dump(config, f, default_flow_style=False)
+    # zps.append(2) # Form V
+    # sgs.append(14)
 
-                ind += 1
+
+    for zp, sg in zip(zps, sgs):
+        ind = 0
+        for seed_ind in np.arange(ind, ind + n_parallel):
+            seed_ind = int(seed_ind)
+            config = deepcopy(base)
+            config['opt_seed'] = seed_ind
+            config['sgs_to_search'] = [sg]
+            config['zp_to_search'] = [zp]
+            config['run_name'] = f'may_acridine_sg{sg}_zp{zp}_{seed_ind}'
+            config['batch_size'] = bsz
+            config['num_samples'] = int(n_samples/n_parallel)
+
+            config_path = f'{sg}_{zp}_{ind}.yaml'
+            with open(config_path, 'w') as f:
+                yaml.dump(config, f, default_flow_style=False)
+
+            ind += 1
